db3a317e2a5a7ba616f0cd5d30d1ef7329ec9bfc
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-16 09:05:45 +0700

    xong

--- public/vidu2.js
+++ public/vidu2.js
@@ -1,11 +1,18 @@
 const React = React;
 
 class Box extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 0 };
+    }
+    onLog() {
+        console.log(this.props.num);
+    }
     render() {
         return (
             <div>
-                <h3>0</h3>
-                <button onClick={() => console.log(this.props.num)}>Add</button>
+                <h3>{this.state.value}</h3>
+                <button onClick={() => this.onLog()}>Add</button>
             </div>
         );
     }
